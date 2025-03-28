2e708df88b759f9110c1c5d223fd0a8f3cf5c9e2
David Semke <david@semke.ca>
2025-03-27 23:09:01 -0700

    Fix(style): Restore typing animation of index page title's first line

--- src/components/page/IndexPage.tsx
+++ src/components/page/IndexPage.tsx
@@ -22,7 +22,7 @@ export default function IndexPage() {
   return (
     <main className="flex flex-col items-center justify-center gap-8 pb-20">
       <h1 className="flex flex-col items-center bg-linear-to-r from-primary to-secondary bg-clip-text py-4 text-center text-transparent">
-          <span className="animate-typingFull overflow-hidden whitespace-nowrap border-r-4 border-accent md:border-r-8">
+          <span className="animate-typing100 overflow-hidden whitespace-nowrap border-r-4 border-accent md:border-r-8">
             Hi! I'm David, a Full
           </span>
           <span className="animate-typing85 overflow-hidden whitespace-nowrap border-r-4 border-accent md:border-r-8">
